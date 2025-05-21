fdef76e1ccc1093f183c20f33be80cd52303a615
chaithramr <mrchaitra34@gmail.com>
2025-05-21 14:13:32 -0700

    Add new files

---
+++ datatype.py
@@ -0,0 +1,13 @@
+# Arithmetic Practice
+num1 = 10
+num2 = 5
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(f"Addition: {num1} + {num2} = {addition}")
+print(f"Subtraction: {num1} - {num2} = {subtraction}")
+print(f"Multiplication: {num1} * {num2} = {multiplication}")
+print(f"Division: {num1} / {num2} = {division}")
