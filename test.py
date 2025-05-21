bb8e0565216af385016f5685d99abec06ef760b7
chaithramr <mrchaitra34@gmail.com>
2025-05-20 19:53:08 -0700

    First code commit.

---
+++ test.py
@@ -0,0 +1,16 @@
+print("hello world!")
+name="John"
+age=30
+print(f"Name: {name}, Age: {age}")
+print("hello world!")
+print("hello world!")
+# for i in range(5):
+#     print(i)
+# while True:
+#     print("Infinite loop")
+#     break
+
+print("hello world!")
+letter = ("chaithra")
+for i in letter:
+    print(i)
